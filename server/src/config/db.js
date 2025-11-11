22f82146e92c3b6655c44ea4bc0f6612ae9e5d75
USUARIO <USUARIO@RTM-UIP-01E.UNMSM.pe>
2025-11-11 16:55:54 -0500

    Se agrega node_modules a .gitignore

--- server/src/config/db.js
+++ server/src/config/db.js
@@ -6,7 +6,7 @@ const sequelize = new Sequelize(
   process.env.DB_USER,
   process.env.DB_PASS,
   {
-    host: process.env.DB_HOST,
+    host: "db",
     dialect: 'postgres',
     logging: false
   }
